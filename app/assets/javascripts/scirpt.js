4c97f91363167df26e00a08076f32a9a77af9acb
KoanBrown <koan.brown@gmail.com>
2015-08-23 17:59:34 -0700

    added links to authors

--- app/assets/javascripts/scirpt.js
+++ app/assets/javascripts/scirpt.js
@@ -8,31 +8,30 @@ $(document).ready(function(){
   var bard = gon.bard;
   var author = gon.author;
 
-  $('#script').fadeIn(1000).html('"' + script + '"');
+  $('#script').html('"' + script + '"').hide().fadeIn('slow');
 
   $('.btn-bard').click(function(){
     if( bard == true ){
       $('#answer').html('Correct');
-      $('#author').html('Author:' + author);
+      $('#author').html("Author: <a href='https://twitter.com/" + author + "'>" + author + "</a>");
     }else{
       $('#answer').html('Shakespeare says "Alas, incorrect."');
-      $('#author').html('Author:' + author);
+      $('#author').html("Author: <a href='https://twitter.com/" + author + "'>" + author + "</a>");
     }
   });
 
   $('.btn-tweet').on("click", function(){
     if( bard == false ){
       $('#answer').html('Correct');
-      $('#author').html('Author:' + author);
-      // $('#author').html('Author: <a href="https://twitter.com/' + author'"' + author '</a>');
+      $('#author').html("Author: <a href='https://twitter.com/" + author + "'>" + author + "</a>");
     }else{
       $('#answer').html('Shakespeare says "Incorrect, damn spot."');
-      $('#author').html('Author:' + author);
+      $('#author').html("Author: <a href='https://twitter.com/" + author + "'>" + author + "</a>");
     }
   });
 
   $(".dismiss").click(function(){
-    location.reload().fadeIn("slow");
+    location.reload();
   });
 
 });
